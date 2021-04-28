f70550b82c69d8a67a4daab1cd8d6d8ce25a5216
PabloMatev <57010721+PabloMatev@users.noreply.github.com>
2021-04-28 11:19:59 +0200

    init our notes project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Particles from "react-particles-js";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 15,
+      density: {
+        enable: true,
+        value_area: 100,
+      },
+    },
+  },
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+      </div>
+    );
+  }
 }
 
 export default App;
